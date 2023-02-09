c8477b3c8ab75f9ed72769819c63498489b1e9df
emystery <alfredo.burnay@gmail.com>
2023-02-09 17:56:31 +0000

    Added "edit card" functionality

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -21,6 +21,47 @@ class Card {
         this.type = type;
     }
 
+    static async edit(newInfo) {
+        try {
+            // Checking if card exist to edit the card
+            let [dbCards] =
+                await pool.query("Select * from cards where crd_id=?",
+                    [newInfo.id]);
+            if (dbCards.length == 0) {
+                return {
+                    status: 404, result: {
+                        msg: "No card found with that ID"
+                    }
+                };
+            };
+
+            // Checking if the new name does not exist (excludes the card
+            // we are editing since we might not want to change the name )
+            [dbCards] =
+                await pool.query("Select * from cards where crd_name=? and crd_id!=?",
+                    [newInfo.name, newInfo.id]);
+            if (dbCards.length)
+                return {
+                    status: 400, result: [{
+                        location: "body", param: "name",
+                        msg: "Another card already has that name"
+                    }]
+                };
+            // We are considering all values will be changing
+            let [result] =
+                await pool.query(`update cards 
+                set crd_name=?, crd_img_url=?, crd_lore=?, 
+                crd_description=?, crd_level=?, crd_cost=?, crd_timeout=?, 
+                crd_max_usage=?, crd_type=? where crd_id=?`, [newInfo.name, newInfo.url, newInfo.lore,
+                newInfo.description, newInfo.level, newInfo.cost, newInfo.timeout,
+                newInfo.maxUsage, newInfo.type, newInfo.id]);
+            return { status: 200, result: { msg: "Card edited" } }
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
+
     static async filterByLoreOrDescription(text) {
         try {
             let result = [];
--- routes/cardsRoutes.js
+++ routes/cardsRoutes.js
@@ -43,13 +43,15 @@ router.get('/:id', async function (req, res, next) {
 });
 
 const { body, validationResult } = require('express-validator');
-router.post("/",
-    body('name').isLength({ min: 4, max: 60 })
-        .withMessage('Name must have between 4 and 60 characters'),
-    body('level').isInt({ min: 0 })
-        .withMessage('Level must be a non negative integer number'),
-    body('type').isInt({ min: 1 })
-        .withMessage('Type must be a positive integer number'),
+
+const cardValidations = [body('name').isLength({ min: 4, max: 60 })
+    .withMessage('Name must have between 4 and 60 characters'),
+body('level').isInt({ min: 0 })
+    .withMessage('Level must be a non negative integer number'),
+body('type').isInt({ min: 1 })
+    .withMessage('Type must be a positive integer number')];
+
+router.post("/", ...cardValidations,
     async function (req, res, next) {
         try {
             console.log("Save card with name " + req.body.name);
@@ -59,6 +61,22 @@ router.post("/",
                 return res.status(400).json(valid.array());
             }
             let result = await Card.save(req.body);
+            res.status(result.status).send(result.result);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    });
+
+router.put("/", ...cardValidations,
+    async function (req, res, next) {
+        try {
+            console.log("Edit card with id " + req.body.id);
+            const valid = validationResult(req);
+            if (!valid.isEmpty()) {
+                return res.status(400).json(valid.array());
+            }
+            let result = await Card.edit(req.body);
             res.status(result.status).send(result.result);
         } catch (err) {
             console.log(err);
